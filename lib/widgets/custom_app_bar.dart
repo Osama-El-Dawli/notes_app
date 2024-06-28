2e53b45bad4c430fa3eb6d21d6001f1f49c9927f
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-06-28 16:45:33 +0300

    create custom app bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notes/widgets/custom_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'Notes App',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        CustomIcon(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.search,
+            size: 28,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/custom_icon.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({
+    super.key,
+    required this.icon, required this.onPressed,
+  });
+  
+  final Icon icon;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(.05),
+      ),
+      child: Center(
+        child: IconButton(
+          onPressed: onPressed,
+          icon: icon,
+        ),
+      ),
+    );
+  }
+}
