122b7246073c6698fdba66d333454bce2d6efcb4
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-06-29 06:27:38 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int colot;
+
+  NoteModel(this.date, this.colot, {required this.title, required this.subTitle});
+}
