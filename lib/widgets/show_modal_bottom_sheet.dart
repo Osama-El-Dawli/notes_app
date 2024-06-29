9c99a999ffe2d29d50e32b51d7566f2a5d155f82
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-06-29 05:26:29 +0300

    create custom text field

--- lib/widgets/show_modal_bottom_sheet.dart
+++ lib/widgets/show_modal_bottom_sheet.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notes/widgets/custom_text_field.dart';
 
 class ShowModalBottomSheet extends StatelessWidget {
   const ShowModalBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Column(
+      children: [
+        SizedBox(
+          height: 32,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: CustomTextField(),
+        ),
+      ],
+    );
   }
 }
