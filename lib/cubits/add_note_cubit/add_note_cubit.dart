ef23b379120459ca7ee92cbb8853fbf5b149c186
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-07-12 16:31:00 +0300

    Add functionality to add a note to Hive database
    This commit adds the ability to add a note to the Hive database using the `addNote` method in the `AddNoteCubit`. The method takes a `NoteModel` object and adds it to the `noteBox` in Hive. If successful, the state is updated to `AddNoteSuccess`, otherwise `AddNoteFailure` is emitted with the error message.

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notes/consts.dart';
+import 'package:notes/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      await noteBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
 }
