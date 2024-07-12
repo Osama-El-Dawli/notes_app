3d15ac97625c7240d79f77e25c4747da358c62cf
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-07-12 17:05:27 +0300

    feat: Add note form and modal bottom sheet with bloc implementation
    - Add `AddNoteCubit` to handle adding notes
    - Implement `AddNoteForm` widget with `CustomTextField` and `CustomButton`
    - Implement `ShowModalBottomSheet` widget to show the `AddNoteForm` in a modal bottom sheet
    - Add `modal_progress_hud_nsn` package for showing progress indicator while adding notes
    - Implement `SimpleBlocObserver` to observe bloc changes
    - Update `main.dart` to use

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes/consts.dart';
+import 'package:notes/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes/models/note_model.dart';
+import 'package:notes/simple_bloc_observer.dart';
 import 'package:notes/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Bloc.observer = SimpleBlocObserver();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
@@ -16,13 +20,20 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
   }
 }
