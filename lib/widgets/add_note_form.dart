3769a9f81fe00adb25693d23c0a2049c6fb79a59
Osama-El-Dawli <osamasaeed283@gmail.com>
2024-07-13 02:56:28 +0300

    Refactor add note form and button
    - Refactored the add note form and button widgets to improve code readability and maintainability.
    - Added a loading indicator to the custom button widget to indicate when a note is being added.
    - Replaced the modal progress HUD widget with an AbsorbPointer widget in the show modal bottom sheet widget to prevent user interaction while a note is being added.

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -47,24 +47,29 @@ class _AddNoteFormState extends State<AddNoteForm> {
             maxLines: 5,
           ),
           const SizedBox(
-            height: 120,
+            height: 28,
           ),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CustomButton(
+                isLoading: state is AddNoteLoading ? true : false,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
 
-                var noteModel = NoteModel(
-                    date: DateTime.now().toString(),
-                    color: Colors.blue.value,
-                    title: title!,
-                    subTitle: subTitle!);
+                    var noteModel = NoteModel(
+                        date: DateTime.now().toString(),
+                        color: Colors.blue.value,
+                        title: title!,
+                        subTitle: subTitle!);
 
-                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
+                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              );
             },
           ),
           const SizedBox(
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:notes/consts.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
   final void Function()? onTap;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
@@ -17,15 +18,24 @@ class CustomButton extends StatelessWidget {
           color: kPrimaryColor,
           borderRadius: BorderRadius.circular(8),
         ),
-        child: const Center(
-            child: Text(
-          'Add',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        )),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                  height: 28,
+                  width: 28,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+        ),
       ),
     );
   }
